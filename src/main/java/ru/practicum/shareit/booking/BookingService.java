48d7f098adbfd4d5e3012fa84da4ee2c613c9e8f
михаил <mihail@MacBook-Air-mihail.local>
2025-11-25 17:14:00 -0800

    Cпринт 15 пробный вариант 3

--- src/main/java/ru/practicum/shareit/booking/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/BookingService.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class BookingService {
     private final BookingRepository bookingRepository;
