4ca61e4f3c3bb3409df65681ba1f015b7e4917c1
михаил <mihail@MacBook-Air-mihail.local>
2025-12-01 22:09:28 -0800

    исправил транзакции

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "bookings")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Booking {
@@ -30,4 +32,34 @@ public class Booking {
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private BookingStatus status = BookingStatus.WAITING;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return Objects.equals(id, booking.id) &&
+                Objects.equals(itemId, booking.itemId) &&
+                Objects.equals(bookerId, booking.bookerId) &&
+                Objects.equals(start, booking.start) &&
+                Objects.equals(end, booking.end) &&
+                status == booking.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, itemId, bookerId, start, end, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", itemId=" + itemId +
+                ", bookerId=" + bookerId +
+                ", start=" + start +
+                ", end=" + end +
+                ", status=" + status +
+                '}';
+    }
 }
